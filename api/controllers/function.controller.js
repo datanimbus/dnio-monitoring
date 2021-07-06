2a04a41af1e1dcf08e000417a6b302a6cf14d416
Jugnu Agrawal <jugnu@appveen.com>
2021-07-06 22:58:52 +0530

    api fixed

--- api/controllers/function.controller.js
+++ api/controllers/function.controller.js
@@ -4,7 +4,7 @@ const fetData = require('../../util/fetchData');
 
 e.functionConsoleLogs = function (req, res) {
 	let app = req.swagger.params.app.value;
-	let functionId = req.swagger.params.function.value;
+	let faasId = req.swagger.params.faasId.value;
 	let filter = req.swagger.params.filter.value;
 	if (!filter) {
 		filter = {};
@@ -13,14 +13,14 @@ e.functionConsoleLogs = function (req, res) {
 		filter = JSON.parse(filter);
 	}
 	filter['context.app'] = app;
-	filter['context.functionId'] = functionId;
-	req.swagger.params.function.value = JSON.stringify(filter);
-	let colName = 'function.console.logs';
+	filter['context.faasId'] = faasId;
+	req.swagger.params.filter.value = JSON.stringify(filter);
+	let colName = 'faas.console.logs';
 	fetData.index(req, res, colName);
 };
 e.functionConsoleLogsCount = function (req, res) {
 	let app = req.swagger.params.app.value;
-	let functionId = req.swagger.params.function.value;
+	let faasId = req.swagger.params.faasId.value;
 	let filter = req.swagger.params.filter.value;
 	if (!filter) {
 		filter = {};
@@ -29,9 +29,9 @@ e.functionConsoleLogsCount = function (req, res) {
 		filter = JSON.parse(filter);
 	}
 	filter['context.app'] = app;
-	filter['context.functionId'] = functionId;
-	req.swagger.params.function.value = JSON.stringify(filter);
-	let colName = 'function.console.logs';
+	filter['context.faasId'] = faasId;
+	req.swagger.params.filter.value = JSON.stringify(filter);
+	let colName = 'faas.console.logs';
 	fetData.count(req, res, colName);
 };
 
