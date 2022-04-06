6b94dd9ab1ba61dd493a84ba1208e7f9236dff97
Jugnu Agrawal <jugnu@appveen.com>
2022-04-06 12:54:32 +0530

    safeLoad => load

--- app.js
+++ app.js
@@ -100,7 +100,7 @@ mongoose.connect(mongoLogsDB, conf.mongoOptionsForLogDb, (err) => {
 
 				// The Swagger document (require it, build it programmatically, fetch it from a URL, ...)
 				var spec = fs.readFileSync(path.join(__dirname, 'api/swagger/swagger.yaml'), 'utf8');
-				var swaggerDoc = jsyaml.safeLoad(spec);
+				var swaggerDoc = jsyaml.load(spec);
 
 				swaggerTools.initializeMiddleware(swaggerDoc, function (middleware) {
 
