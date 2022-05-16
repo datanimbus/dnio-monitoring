14d03611411c8e17d5b8911ef94854a205ddd7f4
Jugnu Agrawal <jugnu@appveen.com>
2022-05-16 18:16:30 +0530

    Jenkins Stage Builds

---
+++ scripts/create_tag.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+cDate=`date +%Y.%m.%d.%H.%M`
+
+
+
+TAG=$RELEASE"_"$cDate
+if [ $tag == 'dev' ]; then
+
+    echo "****************************************************"
+    echo "data.stack:mon :: Default Tag Found, Creating new TAG :: $TAG"
+    echo "****************************************************"
+
+    echo $TAG > CURRENT_MON
+
+else
+    echo "****************************************************"
+    echo "data.stack:mon :: User's Tag Found :: $tag"
+    echo "****************************************************"
+
+    echo $tag > CURRENT_MON
+fi
