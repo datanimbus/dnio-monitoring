eb01ab20ccf070c63c486d12b989a0a277a7289d
Jugnu Agrawal <jugnu@appveen.com>
2022-08-18 20:57:19 +0530

    SMCrud fix

--- api/controllers/uiLog.controller.js
+++ api/controllers/uiLog.controller.js
@@ -2,9 +2,9 @@
 
 const mongoose = require('mongoose');
 const definition = require('../helpers/uiLogs.definition');
-const SMCrud = require('@appveen/swagger-mongoose-crud');
+const { SMCrud, MakeSchema } = require('@appveen/swagger-mongoose-crud');
 const utils = require('@appveen/utils');
-const schema = new mongoose.Schema(definition);
+const schema = MakeSchema(definition);
 const logger = global.logger;
 var options = {
 	logger: logger,
