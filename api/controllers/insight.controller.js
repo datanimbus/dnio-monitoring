9ef84058f5269bba87669900483a654cfb9b4d5a
Jugnu Agrawal <jugnu@appveen.com>
2022-10-20 21:19:46 +0530

    index creation code change

--- api/controllers/insight.controller.js
+++ api/controllers/insight.controller.js
@@ -5,12 +5,16 @@ const logger = global.logger;
 
 const fetData = require('../../util/fetchData');
 
-try {
-	mongoose.connection.db.collection('user.logs').createIndex({ '_metadata.createdAt': 1 });
-	mongoose.connection.db.collection('group.logs').createIndex({ '_metadata.createdAt': 1 });
-} catch (err) {
-	logger.error(err);
-}
+(async () => {
+	try {
+		logger.info('Trying to create Index For user.logs');
+		mongoose.connection.db.collection('user.logs').createIndex({ '_metadata.createdAt': 1 });
+		logger.info('Trying to create Index For group.logs');
+		mongoose.connection.db.collection('group.logs').createIndex({ '_metadata.createdAt': 1 });
+	} catch (err) {
+		logger.error(err);
+	}
+})();
 
 
 e.userInsight = function (req, res) {
